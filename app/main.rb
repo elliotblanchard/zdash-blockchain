f0d700693ddd1ea3e70fb899d1d0d68315ecbe0b
Elliot Blanchard <git@elliotblanchard.com>
2020-12-30 13:21:48 -0500

    Got rpcjson Gem working for results

--- app/main.rb
+++ app/main.rb
@@ -3,21 +3,25 @@ require 'pry'
 
 require 'colorize'
 #require 'bitcoind'
+require 'rpcjson'
 require 'net/http'
 require 'uri'
 require 'json'
 #require 'curl'
 
 =begin
+request_string = '{"jsonrpc": "1.0", "id":"curltest", "method": "getinfo", "params": [] }'
+
 uri = URI.parse("http://192.168.1.158:8232/")
 request = Net::HTTP::Post.new(uri)
 request.basic_auth("samwellhouston", "silversandyblocks")
 request.content_type = "text/plain"
-request.body = JSON.dump({
-  "jsonrpc" => "1.0",
-  "method" => "getinfo",
-  "params" => "[]",
-})
+request.body = request_string.unpack("B*").to_s
+#request.body = JSON.dump({
+#  "jsonrpc" => "1.0",
+#  "method" => "getinfo",
+#  "params" => "[]",
+#})
 
 req_options = {
   use_ssl: uri.scheme == "https",
@@ -31,13 +35,14 @@ end
 # response.code
 # response.body
 
+=begin
 curl = CURL.new
 #page = curl.get("http://google.com")
 
 
 client = CoinRPC::Client.new("http://samwellhouston:silversandyblocks@192.168.1.158:8232")
 puts client.getblockchaininfo
-=end
+
 
 #output = `curl -u samwellhouston:silversandyblocks silversandyblocks --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "getinfo", "params": [] }' -H 'content-type: text/plain;' 192.168.1.158:8232/`
 block_id = "00000000febc373a1da2bd9f887b105ad79ddc26ac26c2b28652d64e5207c5b5"
@@ -48,5 +53,13 @@ puts "Sample transaction via getblock: #{tx_hash}"
 output = `curl -u samwellhouston:silversandyblocks silversandyblocks --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "getrawtransaction", "params": ["#{tx_hash}",1,"#{block_id}"] }' -H 'content-type: text/plain;' 192.168.1.158:8232/`
 parsed = JSON.parse(output)
 puts "Sample txid via getrawtransaction: #{parsed['result']['txid']}"
+=end
+
+zc = RPC::JSON::Client.new 'http://samwellhouston:silversandyblocks@192.168.1.158:8232', 1.1
+puts "Server info is: #{zc.getinfo}"
+block_id = "00000000febc373a1da2bd9f887b105ad79ddc26ac26c2b28652d64e5207c5b5"
+res = zc.getblock(block_id, 1)
+tx_hash = res['tx'][0]
+puts "Sample transaction via getblock: #{tx_hash}"
 
 #binding.pry
