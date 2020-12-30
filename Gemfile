faf36ea5925467ed56c042dcee879ca1cc6bf52e
Elliot Blanchard <git@elliotblanchard.com>
2020-12-29 23:17:43 -0500

    Interim solution using command line to curl

--- Gemfile
+++ Gemfile
@@ -1,14 +1,16 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '2.5.5'
+ruby '2.6.1'
 
 gem "colorize", "~> 0.8.1"
 gem 'activerecord'
-#gem 'standalone_migrations'
+gem 'standalone_migrations'
+#gem 'activesupport', '~> 3.0.6'
 gem 'pry', '~> 0.13.1'
-gem 'curl', '~> 0.0.9'
-gem 'ramontayag-bitcoin-client', '~> 0.1.0'
+#gem 'bitcoind', '~> 0.0.1'
+#gem 'curl', '~> 0.0.9'
+#gem 'ramontayag-bitcoin-client', '~> 0.1.0'
 #gem 'bitcoin-client', '~> 0.0.3'
 #gem 'bitcoiner', '~> 0.2.1'
 #gem 'coinable', '~> 0.0.2'
@@ -16,4 +18,4 @@ gem 'ramontayag-bitcoin-client', '~> 0.1.0'
 #gem 'cryptobank', '~> 0.1.3'
 
 # Use postgresql as the database for Active Record
-# gem 'pg', '>= 0.18', '< 2.0'
+gem 'pg', '>= 0.18', '< 2.0'
--- app/main.rb
+++ app/main.rb
@@ -2,20 +2,19 @@ require 'active_record'
 require 'pry'
 
 require 'colorize'
-#require 'bitcoin'
+#require 'bitcoind'
 require 'net/http'
 require 'uri'
 require 'json'
 #require 'curl'
 
 =begin
-uri = URI.parse("http://127.0.0.1:8232/")
+uri = URI.parse("http://192.168.1.158:8232/")
 request = Net::HTTP::Post.new(uri)
 request.basic_auth("samwellhouston", "silversandyblocks")
 request.content_type = "text/plain"
 request.body = JSON.dump({
   "jsonrpc" => "1.0",
-  "id" => "curltest",
   "method" => "getinfo",
   "params" => "[]",
 })
@@ -28,16 +27,22 @@ response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
   http.request(request)
 end
 
+# curl -u samwellhouston:silversandyblocks silversandyblocks --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "getinfo", "params": [] }' -H 'content-type: text/plain;' 192.168.1.158:8232/
 # response.code
 # response.body
 
-#client = Bitcoin::Client.new('samwellhouston', 'silversandyblocks', :host => '127.0.0.1')
-#client.port = 8232
-=end
-
-=begin
 curl = CURL.new
 #page = curl.get("http://google.com")
+
+
+client = CoinRPC::Client.new("http://samwellhouston:silversandyblocks@192.168.1.158:8232")
+puts client.getblockchaininfo
 =end
 
-binding.pry
+#output = `curl -u samwellhouston:silversandyblocks silversandyblocks --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "getinfo", "params": [] }' -H 'content-type: text/plain;' 192.168.1.158:8232/`
+output = `curl -u samwellhouston:silversandyblocks silversandyblocks --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "getblock", "params": ["00000000febc373a1da2bd9f887b105ad79ddc26ac26c2b28652d64e5207c5b5"] }' -H 'content-type: text/plain;' 192.168.1.158:8232/`
+#output = `curl -u samwellhouston:silversandyblocks silversandyblocks --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "getrawtransaction", "params": ["d1510ab0f419a44e8b807eff96fd8a4c0115a46071ab19133b5f1e8bd2263244"] }' -H 'content-type: text/plain;' 192.168.1.158:8232/`
+parsed = JSON.parse(output)
+puts parsed
+
+#binding.pry
