3f60ae4bb29b20c6761f902ff16d15cad8ed7673
elliotblanchard <github@elliotblanchard.com>
2020-12-29 21:57:38 -0500

    Recent work

--- app/main.rb
+++ app/main.rb
@@ -2,19 +2,22 @@ require 'active_record'
 require 'pry'
 
 require 'colorize'
-
+#require 'bitcoin'
 require 'net/http'
 require 'uri'
 require 'json'
+#require 'curl'
 
+=begin
 uri = URI.parse("http://127.0.0.1:8232/")
 request = Net::HTTP::Post.new(uri)
-request.content_type = "text/plain;"
+request.basic_auth("samwellhouston", "silversandyblocks")
+request.content_type = "text/plain"
 request.body = JSON.dump({
   "jsonrpc" => "1.0",
   "id" => "curltest",
-  "method" => "getblockcount",
-  "params" => "",
+  "method" => "getinfo",
+  "params" => "[]",
 })
 
 req_options = {
@@ -27,5 +30,14 @@ end
 
 # response.code
 # response.body
+
+#client = Bitcoin::Client.new('samwellhouston', 'silversandyblocks', :host => '127.0.0.1')
+#client.port = 8232
+=end
+
+=begin
+curl = CURL.new
+#page = curl.get("http://google.com")
+=end
 
 binding.pry
