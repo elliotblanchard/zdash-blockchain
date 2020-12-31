1293faf163d8bff3c2a6c648bcb364bc73160ca5
Elliot Blanchard <git@elliotblanchard.com>
2020-12-31 12:22:38 -0500

    Connected to main db

--- app/main.rb
+++ app/main.rb
@@ -1,65 +1,29 @@
 require 'active_record'
+require 'json'
 require 'pry'
-
 require 'colorize'
-#require 'bitcoind'
 require 'rpcjson'
 require 'net/http'
 require 'uri'
-require 'json'
-#require 'curl'
+require_relative './models/transaction'
+require_relative './helpers/classify'
 
-=begin
-request_string = '{"jsonrpc": "1.0", "id":"curltest", "method": "getinfo", "params": [] }'
-
-uri = URI.parse("http://192.168.1.158:8232/")
-request = Net::HTTP::Post.new(uri)
-request.basic_auth("samwellhouston", "silversandyblocks")
-request.content_type = "text/plain"
-request.body = request_string.unpack("B*").to_s
-#request.body = JSON.dump({
-#  "jsonrpc" => "1.0",
-#  "method" => "getinfo",
-#  "params" => "[]",
-#})
-
-req_options = {
-  use_ssl: uri.scheme == "https",
-}
-
-response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
-  http.request(request)
+def db_configuration
+  db_configuration_file = File.join(File.expand_path('..', __FILE__), '..', 'db', 'config.yml')
+  YAML.load(File.read(db_configuration_file))
 end
 
-# curl -u samwellhouston:silversandyblocks silversandyblocks --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "getinfo", "params": [] }' -H 'content-type: text/plain;' 192.168.1.158:8232/
-# response.code
-# response.body
-
-=begin
-curl = CURL.new
-#page = curl.get("http://google.com")
-
-
-client = CoinRPC::Client.new("http://samwellhouston:silversandyblocks@192.168.1.158:8232")
-puts client.getblockchaininfo
-
-
-#output = `curl -u samwellhouston:silversandyblocks silversandyblocks --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "getinfo", "params": [] }' -H 'content-type: text/plain;' 192.168.1.158:8232/`
-block_id = "00000000febc373a1da2bd9f887b105ad79ddc26ac26c2b28652d64e5207c5b5"
-output = `curl -u samwellhouston:silversandyblocks silversandyblocks --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "getblock", "params": ["#{block_id}", 1] }' -H 'content-type: text/plain;' 192.168.1.158:8232/`
-parsed = JSON.parse(output)
-tx_hash = parsed['result']['tx'][0]
-puts "Sample transaction via getblock: #{tx_hash}"
-output = `curl -u samwellhouston:silversandyblocks silversandyblocks --data-binary '{"jsonrpc": "1.0", "id":"curltest", "method": "getrawtransaction", "params": ["#{tx_hash}",1,"#{block_id}"] }' -H 'content-type: text/plain;' 192.168.1.158:8232/`
-parsed = JSON.parse(output)
-puts "Sample txid via getrawtransaction: #{parsed['result']['txid']}"
-=end
-
+# Establish connection to Zcash RPC server and database
 zc = RPC::JSON::Client.new 'http://samwellhouston:silversandyblocks@192.168.1.158:8232', 1.1
+ActiveRecord::Base.establish_connection(db_configuration['development'])
+
 puts "Server info is: #{zc.getinfo}"
 block_id = "00000000febc373a1da2bd9f887b105ad79ddc26ac26c2b28652d64e5207c5b5"
 res = zc.getblock(block_id, 1)
 tx_hash = res['tx'][0]
 puts "Sample transaction via getblock: #{tx_hash}"
+res = zc.getrawtransaction(tx_hash,1,block_id)
+tx_id = res['txid']
+puts "Sample transaction via getrawtransaction: #{tx_id}"
 
-#binding.pry
+binding.pry
