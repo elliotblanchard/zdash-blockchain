12c5a1b3d991ac243bea318eabb76ed24a5323f2
Elliot Blanchard <git@elliotblanchard.com>
2020-12-31 14:56:49 -0500

    Basic functionality implemented

--- app/main.rb
+++ app/main.rb
@@ -13,17 +13,60 @@ def db_configuration
   YAML.load(File.read(db_configuration_file))
 end
 
+def save_transaction(current_transaction, current_block, i)
+
+  t = Transaction.create(
+    zhash: current_transaction['txid'],
+    mainChain: nil,
+    fee: nil,
+    ttype: nil,
+    shielded: nil,
+    index: nil,
+    blockHash: current_block['hash'],
+    blockHeight: i,
+    version: current_transaction['version'],
+    lockTime: current_transaction['locktime'],
+    timestamp: current_transaction['time'],
+    time: nil,
+    vin: current_transaction['vin'],
+    vout: current_transaction['vout'],
+    vjoinsplit: current_transaction['vjoinsplit'],
+    vShieldedOutput: nil,
+    vShieldedSpend: nil,
+    valueBalance: nil,
+    value: nil,
+    outputValue: nil,
+    shieldedValue: nil,
+    overwintered: nil
+  )
+
+  category = Classify.classify_transaction(t)
+  t.destroy unless t.update(category: category) # Because duplicate zhash
+  print "#{current_transaction['txid']} not saved #{t.errors.messages}".colorize(:red) unless t.valid?
+
+end
+
 # Establish connection to Zcash RPC server and database
 zc = RPC::JSON::Client.new 'http://samwellhouston:silversandyblocks@192.168.1.158:8232', 1.1
 ActiveRecord::Base.establish_connection(db_configuration['development'])
 
-puts "Server info is: #{zc.getinfo}"
-block_id = "00000000febc373a1da2bd9f887b105ad79ddc26ac26c2b28652d64e5207c5b5"
-res = zc.getblock(block_id, 1)
-tx_hash = res['tx'][0]
-puts "Sample transaction via getblock: #{tx_hash}"
-res = zc.getrawtransaction(tx_hash,1,block_id)
-tx_id = res['txid']
-puts "Sample transaction via getrawtransaction: #{tx_id}"
+zc_network = zc.getinfo
+final_block = zc_network["blocks"] - 100 # 100 most recent blocks may not be finalized
 
-binding.pry
+# Main loop: get each block in Zcash blockchain
+(0..final_block).each do |i|
+  current_block = zc.getblock(i.to_s, 1)
+  num_transactions = current_block['tx'].length - 1
+  # Inner loop: get each transaction in this block
+  (0..num_transactions).each do |j|
+    tx_hash = current_block['tx'][j]
+    begin
+      current_transaction = zc.getrawtransaction(tx_hash.to_s, 1)
+      save_transaction(current_transaction, current_block, i)
+      print "For block #{i} / transaction #{j} transaction is: #{current_transaction}\n".colorize(:green)
+    rescue => e
+      print "For block #{i} / transaction #{j} transaction #{tx_hash} not found.\n".colorize(:red)
+    end
+    binding.pry
+  end
+end
